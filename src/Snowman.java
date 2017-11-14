ab7cc3a9986769b92724017d84c0d4ddf2885861
igr <mixa_fc@ukr.net>
2017-11-14 21:30:31 +0200

    снеговик начало

---
+++ src/Snowman.java
@@ -0,0 +1,29 @@
+import javafx.scene.shape.Circle;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Snowman {
+    private int height;
+    private int width;
+
+    private ArrayList<Circle> circleArrayList;
+    private ArrayList<Circle> headArrayList;
+
+    public void Join (int count, int max, int min){
+        Random random = new Random();
+        int head = min;
+        height = 0;
+        width = min;
+        for (i = 0; i < count; i++){
+            int radius = random.nextInt(max - min) + min;
+        }
+        if (circleArrayList == null) circleArrayList = new ArrayList<>();
+        else circleArrayList.clear();
+        if (headArrayList == null) headArrayList = new ArrayList<>();
+        else headArrayList.clear();
+
+
+    }
+
+}
